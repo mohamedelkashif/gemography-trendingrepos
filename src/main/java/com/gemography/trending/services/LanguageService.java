41dfb26ad37dd28ef80813fe57de1a8eff6c259e
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-12-21 03:47:13 +0200

    reformat code

--- src/main/java/com/gemography/trending/services/LanguageService.java
+++ src/main/java/com/gemography/trending/services/LanguageService.java
@@ -1,11 +1,8 @@
 package com.gemography.trending.services;
 
-import com.gemography.trending.domainobjects.Items;
-import com.gemography.trending.domainobjects.Languages;
 import com.gemography.trending.domainobjects.LanguagesInfo;
 
 import java.io.IOException;
-import java.util.List;
 
 public interface LanguageService {
     LanguagesInfo getTrendingLanguages() throws IOException;
