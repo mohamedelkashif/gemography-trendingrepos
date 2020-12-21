1ba2b28907e64843470f8b8607f678cbe7d9bb17
mohamedelkashif <mohamed_elkashif@windowslive.com>
2020-12-21 03:54:24 +0200

    Adding swagger to the project

--- src/main/java/com/gemography/trending/TrendingApplication.java
+++ src/main/java/com/gemography/trending/TrendingApplication.java
@@ -2,12 +2,38 @@ package com.gemography.trending;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication
 public class TrendingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TrendingApplication.class, args);
+	}
+
+	@Bean
+	public Docket docket()
+	{
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage(getClass().getPackage().getName()))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(generateApiInfo());
+	}
+
+
+	private ApiInfo generateApiInfo()
+	{
+		return new ApiInfo("Gemography backend coding challenge", "This service is part of hiring process on gemography", "Version 1.0 - mw",
+				"urn:tos", "mohammedd.kashiff@gmail.com", "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
 	}
 
 }
--- src/main/java/com/gemography/trending/controllers/LanguageController.java
+++ src/main/java/com/gemography/trending/controllers/LanguageController.java
@@ -1,18 +1,17 @@
 package com.gemography.trending.controllers;
 
-import com.gemography.trending.domainobjects.Items;
 import com.gemography.trending.domainobjects.LanguagesInfo;
 import com.gemography.trending.services.LanguageService;
+import io.swagger.annotations.ApiImplicitParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 
 @RestController
-@RequestMapping("v1/languages")
+@RequestMapping("/api/v1/languages")
 public class LanguageController {
     private LanguageService languageService;
 
@@ -22,6 +21,7 @@ public class LanguageController {
     }
 
     @GetMapping("/")
+    @ApiImplicitParam(name = "Getting trending repositories based on languages")
     public LanguagesInfo getLanguages() throws IOException {
         return languageService.getTrendingLanguages();
     }
